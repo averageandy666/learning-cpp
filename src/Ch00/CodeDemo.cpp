c6fdc22eafe5b1a338642b4fc67964a0406bcdd6
aebeagle <53877078+averageandy666@users.noreply.github.com>
2026-06-01 14:06:21 +0000

    ch00

--- src/Ch00/CodeDemo.cpp
+++ src/Ch00/CodeDemo.cpp
@@ -7,11 +7,13 @@
 int main(){
     float num_1, num_2, result;
 
+    //takes terminal input for 2 numbers
     std::cout << "Enter number 1: " << std::flush;
     std::cin >> num_1;
     std::cout << "Enter number 2: " << std::flush;
     std::cin >> num_2;
     
+    // adds two number inputs
     result = num_1 + num_2;
     
     std::cout << "The result of the addition is " << result << std::endl;
