af05fb0cb2b9210d7bf7adf4a26b4957267f2381
aebeagle <53877078+averageandy666@users.noreply.github.com>
2026-06-02 20:19:06 +0000

    ch05

--- src/Ch05/05_02b/CodeDemo.cpp
+++ src/Ch05/05_02b/CodeDemo.cpp
@@ -4,13 +4,28 @@
 
 #include <iostream>
 
+// Takes arguments by value, so the original variables are not modified
+int square(int x) {
+    return x * x;
+}
+
+// Swaps the values of two integers
+void swap(int& x, int& y) {
+    int temp = x;
+    x = y;
+    y = temp;
+}
+
 int main(){
     int a = 9, b;
     // TODO: square
+    b = square(a);
     std::cout << "a = " << a << ", b = " << b << std::endl;
     // TODO: swap
+    swap(a, b);
     std::cout << "a = " << a << ", b = " << b << std::endl;
     // TODO: swap
+    swap(a, b);
     std::cout << "a = " << a << ", b = " << b << std::endl;
     
     std::cout << std::endl << std::endl;
